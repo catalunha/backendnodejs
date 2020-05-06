990f826ca1bcab168a192e4239e5afa87ba2f621
catalunha <catalunha.mj@gmail.com>
2020-05-06 20:51:59 -0300

    gets e posts

--- index.js
+++ index.js
@@ -2,14 +2,54 @@
 const express = require('express')
 
 const app = express()
+app.use(express.json())
+/**
+ * Metodos HTTP
+ *  GET: Buscar/listar
+ *  POST: Criar
+ *  PUT: Alterar
+ *  DELETE: Deletar
+ */
 
-app.get('/', (request, response) => {
+ /**
+  * Tipos de parametros
+  * Query: Parametros nomeados enviados na rota após o '?' (filtros, paginacao, ) unidos com '&'. 
+  *   app.get('/users', (request, response) => {
+  *   http://localhost:3333/users?name=joao&qde=10
+  *   acessiveis atraves do: request.query
+  * Params: Parametros para identificar recursos
+  *   app.get('/users/:id', (request, response) => {
+  *   http://localhost:3333/users/1
+  * Body: Corpo da requisição para criar ou alterar recursos
+  *   app.post('/users', (request, response) => {
+  *   http://localhost:3333/users com body {...}
+  */
+app.get('/users', (request, response) => {
+  const reqQuery = request.query // http://localhost:3333/users?name=joao&qde=10
+  console.log(reqQuery)
   // return response.send('Hello Word')
   return response.json({
     evento: 'Semana OmniStack 11.0',
     aluno: 'Prof Catalunha',
   })
 })
-
+app.get('/users/:id', (request, response) => {
+  const reqParams = request.params // http://localhost:3333/users/1
+  console.log(reqParams)
+  // return response.send('Hello Word')
+  return response.json({
+    evento: 'Semana OmniStack 11.0',
+    aluno: 'Prof Catalunha',
+  })
+})
+app.post('/users', (request, response) => {
+  const reqBody = request.body // 
+  console.log(reqBody)
+  // return response.send('Hello Word')
+  return response.json({
+    evento: 'Semana OmniStack 11.0',
+    aluno: 'Prof Catalunha',
+  })
+})
 app.listen(3333)
 
