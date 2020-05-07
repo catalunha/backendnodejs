db47f047d37ad55d68b8ab73565a3954159b98bf
catalunha <catalunha.mj@gmail.com>
2020-05-07 17:32:56 -0300

    criacao e listagem das ongs

--- docs.md
+++ docs.md
@@ -11,13 +11,15 @@
   * Query: Parametros nomeados enviados na rota após o '?' (filtros, paginacao, ) unidos com '&'. 
   *   app.get('/users', (request, response) => {
   *   http://localhost:3333/users?name=joao&qde=10
-  *   acessiveis atraves do: request.query
+  *   acessiveis atraves do: const reqQuery = request.query
   * Params: Parametros para identificar recursos
   *   app.get('/users/:id', (request, response) => {
   *   http://localhost:3333/users/1
+  *   acessivel atraves do: const reqParams = request.params
   * Body: Corpo da requisição para criar ou alterar recursos
   *   app.post('/users', (request, response) => {
   *   http://localhost:3333/users com body {...}
+  *   acessivel atraves do: const reqBody = request.body
   */
 
 /**
--- src/routes.js
+++ src/routes.js
@@ -1,33 +1,9 @@
 const express = require('express')
+const OngController = require('./controllers/OngController')
 const routes = express.Router()
 
+routes.post('/ongs', OngController.create)
 
-routes.get('/users', (request, response) => {
-  const reqQuery = request.query // http://localhost:3333/users?name=joao&qde=10
-  console.log(reqQuery)
-  // return response.send('Hello Word')
-  return response.json({
-    evento: 'Semana OmniStack 11.0',
-    aluno: 'Prof Catalunha',
-  })
-})
-routes.get('/users/:id', (request, response) => {
-  const reqParams = request.params // http://localhost:3333/users/1
-  console.log(reqParams)
-  // return response.send('Hello Word')
-  return response.json({
-    evento: 'Semana OmniStack 11.0',
-    aluno: 'Prof Catalunha',
-  })
-})
-routes.post('/users', (request, response) => {
-  const reqBody = request.body // 
-  console.log(reqBody)
-  // return response.send('Hello Word')
-  return response.json({
-    evento: 'Semana OmniStack 11.0',
-    aluno: 'Prof Catalunha.',
-  })
-})
+routes.get('/ongs', OngController.index)
 
 module.exports = routes
