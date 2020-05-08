b7671a2edcf5be778dd2795034db965561a5da66
catalunha <catalunha.mj@gmail.com>
2020-05-08 10:09:27 -0300

    cors em produção

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const routes = require('./routes')
 const app = express()
+// No caso de estar em produção
 // app.use(cors({
 //   origin:'http://meuapp.com'
 // }))
@@ -9,5 +10,3 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.listen(3333)
-
-
